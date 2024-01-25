529925f1651a970005d9010f42017b8dd4e03ba6
abdullah-works <abdullah.worke@gmail.com>
2024-01-25 09:45:20 +0500

    added cards for contact info
    Used ListTile widget as a child for the Card Widget and added two of those. One for mobile number and the second for email address.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -22,8 +22,6 @@ class MainScreen extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 26.0,
-                letterSpacing: 1.5,
-                fontStyle: FontStyle.italic,
               ),
             ),
             Text(
@@ -32,6 +30,54 @@ class MainScreen extends StatelessWidget {
                 color: Colors.white60,
                 fontSize: 16.0,
                 letterSpacing: 3.0,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: 300,
+              child: Divider(
+                thickness: 2,
+                color: Colors.teal,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+              child: ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 24, vertical: 6),
+                leading: Icon(
+                  Icons.call,
+                  size: 28,
+                  color: Color.fromARGB(221, 39, 146, 29),
+                ),
+                title: Text(
+                  '(+92) 310 0918308',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 28, vertical: 5),
+              child: ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 24, vertical: 6),
+                leading: Icon(
+                  Icons.mail,
+                  size: 28,
+                  color: Color.fromARGB(221, 210, 42, 42),
+                ),
+                title: Text(
+                  'abdullah.worke@gmail.com',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
               ),
             ),
           ],
